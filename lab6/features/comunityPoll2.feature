d6ce06bbfafb8657259de745599c211390c38357
Diana <mdiannna@yahoo.com>
2020-11-13 20:46:11 +0200

    Started adding new test for sorting ascending A-Z men's clothes

--- lab6/features/comunityPoll2.feature
+++
@@ -1,8 +0,0 @@
-@CommunityPoll2
-Feature: Community Poll
-
-@Poll
-Scenario: Verify comunity poll feature on men's clothes page
-Given Open men's clothes page
-Then Select option 'Lower price' from the poll options
-Then Validate result community poll
---
+++ lab6/features/step_definitions/stepsPriceAscending.rb
@@ -0,0 +1,27 @@
+require 'selenium-webdriver'
+require 'rubygems'
+require 'rspec'
+
+driver = Selenium::WebDriver.for:chrome                                     # webdriver with chromedriver
+
+
+Given("Open men clothes page") do
+  driver.navigate.to "https://adoring-pasteur-3ae17d.netlify.app/mens.html"         # direct to site
+end
+
+
+# Then("Choose Price(Low-High) option in the Sort By select") do
+Then('Choose Price\(Low-High) option in the Sort By select') do
+
+	# dropDownMenu = @driver.find_element(:class, 'dropDownMenu')
+	dropDownMenu = driver.find_element(:id, 'country1')
+
+	option = Selenium::WebDriver::Support::Select.new(dropDownMenu)
+	option.select_by(:text, 'Name(A - Z)')
+	# option.select_by(:value, '0')
+
+
+	# TODO:!!!! Check if results are sorted!!!
+
+    sleep(1)                                                                # sleep/pause 2 second
+end
