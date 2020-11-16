544e954f275c5a5115d75984fd4529aca5db5165
Diana <mdiannna@yahoo.com>
2020-11-16 17:05:59 +0200

    added tabsProductPage feature

---
+++ lab6/features/step_definitions/tabsProductPage.rb
@@ -0,0 +1,119 @@
+require 'selenium-webdriver'
+require 'rubygems'
+require 'rspec'
+
+driver = Selenium::WebDriver.for:chrome                                     # webdriver with chromedriver
+
+
+Given('Access products page') do
+  driver.navigate.to "https://adoring-pasteur-3ae17d.netlify.app/single.html"         # direct to site
+end
+
+
+Then('Click on "Reviews" tab') do
+	ul = driver.find_element(:class, "resp-tabs-list")
+	lis = ul.find_elements(:class, "resp-tab-item")
+	
+	for li in lis 
+		# log(li.text.downcase)
+		
+		if li.text.downcase=='reviews'
+			li.click()
+		end
+	end
+
+	sleep(1)
+	# tabContainer = driver.find_element(:class, "single_page_agile_its_w3ls")
+	# tabContainer = driver.find_element(:class, "single_page_agile_its_w3ls")
+	# tabContainer = driver.find_element(:css, "div[class='resp-tab-content-active']")
+	tabContainer = driver.find_element(:class, "resp-tab-content-active")
+
+	# log("tabContainer text:", tabContainer.text)
+	# addReview = tabContainer.find_element(:css, "div[class='add-review']")
+	addReview = tabContainer.find_element(:css, "h4")
+	log("addReview text:", addReview.text)
+
+	# check if text "add a review is present in the active tab"
+	expect(addReview.text.downcase).to eq('add a review')
+end
+
+Then('Click on "Information" tab') do
+	ul = driver.find_element(:class, "resp-tabs-list")
+	lis = ul.find_elements(:class, "resp-tab-item")
+	
+	for li in lis 
+		# log(li.text.downcase)
+		
+		if li.text.downcase=='information'
+			li.click()
+		end
+	end
+
+	sleep(1)
+	# tabContainer = driver.find_element(:class, "single_page_agile_its_w3ls")
+	# tabContainer = driver.find_element(:class, "single_page_agile_its_w3ls")
+	# tabContainer = driver.find_element(:css, "div[class='resp-tab-content-active']")
+	tabContainer = driver.find_element(:class, "resp-tab-content-active")
+
+	# log("tabContainer text:", tabContainer.text)
+	# addReview = tabContainer.find_element(:css, "div[class='add-review']")
+	productName = tabContainer.find_element(:css, "h6")
+	log("Product name text:", productName.text)
+
+	# check if text "add a review is present in the active tab"
+	expect(productName.text.downcase).to eq('Big Wing Sneakers (Navy)'.downcase)
+end
+
+Then('Click on "Description" tab') do
+	ul = driver.find_element(:class, "resp-tabs-list")
+	lis = ul.find_elements(:class, "resp-tab-item")
+	
+	for li in lis 
+		# log(li.text.downcase)
+		
+		if li.text.downcase=='description'
+			li.click()
+		end
+	end
+
+	sleep(1)
+	# tabContainer = driver.find_element(:class, "single_page_agile_its_w3ls")
+	# tabContainer = driver.find_element(:class, "single_page_agile_its_w3ls")
+	# tabContainer = driver.find_element(:css, "div[class='resp-tab-content-active']")
+	tabContainer = driver.find_element(:class, "resp-tab-content-active")
+
+	# log("tabContainer text:", tabContainer.text)
+	# addReview = tabContainer.find_element(:css, "div[class='add-review']")
+	title = tabContainer.find_element(:css, "h6")
+	log("Product name text:", title.text)
+
+	# check if text "add a review is present in the active tab"
+	expect(title.text.downcase).to eq('LOREM IPSUM DOLOR SIT AMET'.downcase)
+end
+
+Then('Click on "Information" tab again') do
+	ul = driver.find_element(:class, "resp-tabs-list")
+	lis = ul.find_elements(:class, "resp-tab-item")
+	
+	for li in lis 
+		# log(li.text.downcase)
+		
+		if li.text.downcase=='information'
+			li.click()
+		end
+	end
+
+	sleep(1)
+	# tabContainer = driver.find_element(:class, "single_page_agile_its_w3ls")
+	# tabContainer = driver.find_element(:class, "single_page_agile_its_w3ls")
+	# tabContainer = driver.find_element(:css, "div[class='resp-tab-content-active']")
+	tabContainer = driver.find_element(:class, "resp-tab-content-active")
+
+	# log("tabContainer text:", tabContainer.text)
+	# addReview = tabContainer.find_element(:css, "div[class='add-review']")
+	productName = tabContainer.find_element(:css, "h6")
+	log("Product name text:", productName.text)
+
+	# check if text "add a review is present in the active tab"
+	expect(productName.text.downcase).to eq('Big Wing Sneakers (Navy)'.downcase)
+end
---
+++ lab6/features/tabsProductPage.feature
@@ -0,0 +1,10 @@
+@TabsFunctionality
+Feature: Test tabs functionality in product page
+
+@Tabs
+Scenario: Test if the tabs are working properly in product page
+Given Access products page
+Then Click on "Reviews" tab
+Then Click on "Information" tab
+Then Click on "Description" tab
+Then Click on "Information" tab again
