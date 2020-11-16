b08fe5c9e84c5c89e3ebbd9e89010f56f22dd17e
Diana <mdiannna@yahoo.com>
2020-11-16 21:27:55 +0200

    Finished writeProductReview test

--- lab6/features/step_definitions/writeProductReview.rb
+++ lab6/features/step_definitions/writeProductReview.rb
@@ -10,7 +10,7 @@ Given('Access the products page') do
 end
 
 
-Then('Click on tab "Reviews"') do
+Then('​Click on tab Reviews') do
 	ul = driver.find_element(:class, "resp-tabs-list")
 	lis = ul.find_elements(:class, "resp-tab-item")
 	
@@ -38,22 +38,97 @@ Then('Click on tab "Reviews"') do
 end
 
 
-Then('Write name in the field "Name" (first field)') do
-	pending
+# Then('Write name in the field "Name" (first field)') do
+Then('Write name in the field "Name" \(first field)') do
+	sleep(1)
+	driver.execute_script("window.scrollTo(0, window.scrollY + 500)")
+	sleep(2)
+
+	# input = driver.find_element(:css, "input[name='Name']").send_keys("Name test")
+
+	tabContainer = driver.find_element(:class, "resp-tab-content-active")
+
+	# input = driver.find_element(:css, "input[name='Email']").send_keys("email test")
+    elem = tabContainer.find_element(:name, "Name")
+    # elem.click()
+    elem.clear()
+    elem.send_keys("Test name")
 end
 
 Then('Negative test - write the email without @ in the email field and press enter') do
-	pending
+	input = driver.find_element(:css, "input[name='Name']")
+
+	sleep(1)
+	driver.execute_script("window.scrollTo(0, window.scrollY + 500)")
+	sleep(2)
+
+	# input = driver.find_element(:css, "input[name='Name']").send_keys("Name test")
+
+	tabContainer = driver.find_element(:class, "resp-tab-content-active")
+
+	# input = driver.find_element(:css, "input[name='Email']").send_keys("email test")
+    elem = tabContainer.find_element(:name, "Email")
+    elem.click()
+    elem.clear()
+    elem.send_keys("Bad email")
+    elem.send_keys(:return)
+
+    message = elem.attribute("validationMessage")
+    log("message:", message)
+
+    # asteptam sa apara eroarea care contine ceva cu email adress 
+    expect(message.downcase).to include('email address')
+    # asteptam sa apara eroarea care contine ceva cu @
+    expect(message.downcase).to include('@')
+    # asteptam sa apara eroarea care contine ceva cu missing
+    expect(message.downcase).to include('missing')
 end
 
-Then('Write the correct email in the field "Email" (first field)') do
-	pending
+
+Then('​Write the correct email in the field Email \(first field)') do
+	input = driver.find_element(:css, "input[name='Name']")
+
+	sleep(1)
+	driver.execute_script("window.scrollTo(0, window.scrollY + 500)")
+	sleep(2)
+
+	# input = driver.find_element(:css, "input[name='Name']").send_keys("Name test")
+
+	tabContainer = driver.find_element(:class, "resp-tab-content-active")
+
+	# input = driver.find_element(:css, "input[name='Email']").send_keys("email test")
+    elem = tabContainer.find_element(:name, "Email")
+    elem.click()
+    elem.clear()
+    elem.send_keys("test@test.com")
+    elem.send_keys(:return)
+
+    message = elem.attribute("validationMessage")
+    log("message:", message)
+
+    # asteptam sa apara eroarea care contine ceva cu email adress 
+    expect(message.downcase).to eq("")
 end
+
 
 Then('Write the review in the textarea') do
-	pending
+	tabContainer = driver.find_element(:class, "resp-tab-content-active")
+
+	# input = driver.find_element(:css, "input[name='Email']").send_keys("email test")
+    elem = tabContainer.find_element(:name, "Message")
+    # elem.click()
+    elem.clear()
+    elem.send_keys("Test This is my review")
+    sleep(1)
 end
 
-Then('Click on "SEND" button') do
-	pending
+Then('​Click on SEND button') do
+	tabContainer = driver.find_element(:class, "resp-tab-content-active")
+	# tabContainer.find_element(:css,'[type="submit"]').click                       # click submit form
+	s = tabContainer.find_element(:css,'form').submit()                       # click submit form
+
+	sleep(2)
+
+	page_text = driver.find_element(:css, "body").text()
+	expect(page_text.downcase).not_to include("page not found")
 end
