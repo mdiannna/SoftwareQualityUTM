14101ea2a3ad9293645e9b904e10ab5267acffd7
Diana <mdiannna@yahoo.com>
2020-11-28 11:20:54 +0200

    Lab7 finished todo test

--- lab7/features/googleSearch.feature
+++ lab7/features/googleSearch.feature
@@ -2,9 +2,19 @@
 Feature: Search on google.com
 
 
-@NonExistingData
-Scenario: Enter not existing data means ++++++, 
+@SearchNonExistingData
+Scenario: Enter not existing data means ++++++ in google search 
 Given Go to google.com
-Then Write non existing data ++++++
-Then It should not match anything
-Then It should not display anything
+Then Write non existing data ++++++ in search input
+Then Click search
+Then It should not display any results
+
+
+
+@SearchExistingData
+Scenario: Enter existing data in google search
+Given Go to google.com
+Then Write word "Technical" in search input
+Then Click search
+Then It should display results for "Technical" word search
+Then Results should be similar to term "Technical"
--- lab7/features/step_definitions/stepsGoogleSearch.rb
+++ lab7/features/step_definitions/stepsGoogleSearch.rb
@@ -4,6 +4,7 @@ require 'rspec'
 
 driver = ""
 
+# Before('@SearchNonExistingData') do
 Before('@SearchOnGoogle') do
 	driver = Selenium::WebDriver.for:chrome                                     # webdriver with chromedriver
 end
@@ -13,9 +14,63 @@ Given('Go to google.com') do
   driver.navigate.to "https://www.google.com/"         # direct to site
 end
 
-Then('Write non existing data ++++++') do
- 	# TODO		
-	pending
+Then('Write non existing data ++++++ in search input') do
+ 	search_input = driver.find_element(:class, 'gLFyf')
+ 	search_input.clear()
+ 	search_input.send_keys("++++++")
+ 	sleep(1)
+end
+
+Then('Click search') do	
+ 	# search_button = driver.find_element(:css, "input[class='gNO89b']")
+ 	# # search_button = driver.find_element(:css, "input[area-label='Căutare Google']")
+ 	# search_button.click()
+
+	driver.find_element(:css,'form').submit()                       # click submit form
+
+ 	sleep(2)
+end
+
+Then('It should not display any results') do
+	# result = driver.find_element(:css, "div[class='mw']")
+	result = driver.find_element(:id, "res")
+
+	# expect(result.text()).to include('Căutarea dvs. - ++++++ - nu a returnat niciun document')
+	expect(result.text()).to include('nu a returnat niciun document')
+end
+
+
+Then('Write word "Technical" in search input') do
+	search_input = driver.find_element(:class, 'gLFyf')
+ 	search_input.clear()
+ 	search_input.send_keys("Technical")
+ 	sleep(1)
+end
+
+Then('It should display results for "Technical" word search') do
+	# merge si cu res si cu search
+	result = driver.find_element(:id, "res")
+	# search = driver.find_element(:id, "search")
+
+	log("search text:", result.text())
+
+	# Verificam daca nu se afiseaza "Cautarea nu a returnat niciun document"
+	expect(result.text()).not_to include('nu a returnat niciun document')
+
+	# Verificam daca sunt mai mult de 0 rezultate la cautare
+	search_item_results = result.find_elements(:class, "g")
+	expect(search_item_results.length).not_to eq(0)
+end
+
+Then('Results should be similar to term "Technical"') do
+	result = driver.find_element(:id, "res")
+
+	search_item_results = result.find_elements(:class, "g")
+
+	# Verificam daca primele 5 rezultate contin cuvantul "technical"
+	for search_item in search_item_results[0..5]
+		expect(search_item.text.downcase).to include('technical')
+	end
 end
 
 After('@SearchOnGoogle') do
