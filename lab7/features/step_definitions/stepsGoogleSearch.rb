9fe83963fd3894c532affbd47c41cdd3b09f3f57
Diana <mdiannna@yahoo.com>
2020-11-28 12:38:39 +0200

    Some minor fixes and running on browserstack

--- lab7/features/step_definitions/stepsGoogleSearch.rb
+++ lab7/features/step_definitions/stepsGoogleSearch.rb
@@ -4,9 +4,30 @@ require 'rspec'
 
 driver = ""
 
+USERNAME = "diana367"
+AUTOMATE_KEY = "NmsqJih8tcUphDjkhqqS"
+browserstack_url = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub"
+
+caps = Selenium::WebDriver::Remote::Capabilities.new
+        
+caps['os'] = 'Windows'
+caps['os_version'] = '10'
+caps['browser'] = 'Chrome'
+caps['browser_version'] = '80'
+caps['takesScreenshot'] = true
+        
+caps['name'] = "Test Google Search"
+
+
+
+
 # Before('@SearchNonExistingData') do
 Before('@SearchOnGoogle') do
-	driver = Selenium::WebDriver.for:chrome                                     # webdriver with chromedriver
+	# Running on local:
+	driver = Selenium::WebDriver.for:chrome
+
+	# Running on browserstack
+	# driver = Selenium::WebDriver.for(:remote, :url => browserstack_url, :desired_capabilities => caps)
 end
 
 
@@ -19,26 +40,29 @@ Then('Write non existing data ++++++ in search input') do
  	search_input = driver.find_element(:class, 'gLFyf')
  	search_input.clear()
  	search_input.send_keys("++++++")
- 	sleep(1)
+ 	sleep(0.4)
 end
 
 
 Then('Click search') do	
  	# search_button = driver.find_element(:css, "input[class='gNO89b']")
- 	# # search_button = driver.find_element(:css, "input[area-label='Căutare Google']")
- 	# search_button.click()
+ 	# search_button.click() 
 
 	driver.find_element(:css,'form').submit()                       # click submit form
 
- 	sleep(2)
+ 	sleep(0.8)
 end
 
 
 Then('It should not display any results') do
 	result = driver.find_element(:id, "res")
 
+	# functioneaza doar pentru google ro
 	# expect(result.text()).to include('Căutarea dvs. - ++++++ - nu a returnat niciun document')
-	expect(result.text()).to include('nu a returnat niciun document')
+	# expect(result.text()).to include('nu a returnat niciun document')
+
+	search_item_results = result.find_elements(:class, "g")
+	expect(search_item_results.length).to eq(0)
 end
 
 
@@ -57,8 +81,8 @@ Then('It should display results for "Technical" word search') do
 
 	log("search text:", result.text())
 
-	# Verificam daca nu se afiseaza "Cautarea nu a returnat niciun document"
-	expect(result.text()).not_to include('nu a returnat niciun document')
+	# Verificam daca nu se afiseaza "Cautarea nu a returnat niciun document" - functioneaza doar pentru google ro
+	# expect(result.text()).not_to include('nu a returnat niciun document')
 
 	# Verificam daca sunt mai mult de 0 rezultate la cautare
 	search_item_results = result.find_elements(:class, "g")
@@ -67,13 +91,19 @@ end
 
 
 Then('Results should be similar to term "Technical"') do
+	# result = driver.find_element(:id, "search")
 	result = driver.find_element(:id, "res")
 
 	search_item_results = result.find_elements(:class, "g")
+	# search_item_results = result.find_elements(:class, "rspec")
 
 	# Verificam daca primele 5 rezultate contin cuvantul "technical"
-	for search_item in search_item_results[0..5]
-		expect(search_item.text.downcase).to include('technical')
+	for search_item in search_item_results[1..5]
+
+		log("--- item:", search_item.text())
+		if search_item.text() != ""
+			expect(search_item.text.downcase).to include('technic')
+		end
 	end
 end
 
